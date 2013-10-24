4eb901aff6ffa3812531d5379199a7826113458b
Stanly Lee <stanly.lns@gmail.com>
2013-10-24 22:49:33 +0800

    added Ads

--- gen/com/yuniz/liveframe/R.java
+++ gen/com/yuniz/liveframe/R.java
@@ -18,11 +18,13 @@ public final class R {
         public static final int ic_musbtn=0x7f020004;
     }
     public static final class id {
-        public static final int button1=0x7f060002;
-        public static final int button2=0x7f060001;
-        public static final int button3=0x7f060004;
-        public static final int imageDummy=0x7f060003;
-        public static final int imageView=0x7f060000;
+        public static final int adView=0x7f060006;
+        public static final int button1=0x7f060003;
+        public static final int button2=0x7f060002;
+        public static final int button3=0x7f060005;
+        public static final int imageDummy=0x7f060004;
+        public static final int imageView=0x7f060001;
+        public static final int linearLayoutMain=0x7f060000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
